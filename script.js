d9e1385ce04ef31d5fc0ef2a10891d2510e77fb0
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-13 19:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function firstNonRepeatedChar(str) {
-    let charfrequency ={};
-    for(let char of str){
-        charfrequency[char] = (charfrequency[char] || 0)+1;
-    }
-    for(let char of str){
-        if(charfrequency[char]===1){
+    for (let char of str) {
+        if (str.indexOf(char) === str.lastIndexOf(char)) {
             return char;
         }
     }
     return null;
-    
 }
-   const input = prompt("Enter a string");
-   alert(firstNonRepeatedChar(input));
+
+const input = prompt("Enter a string");
+const result = firstNonRepeatedChar(input);
+
+if (result !== null) {
+    alert(`The first non-repeated character is: ${result}`);
+} else {
+    alert("There is no non-repeated character in the given string.");
+}
 
